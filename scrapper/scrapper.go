7d88df523c213fb6d57864c4f23f3801662dac24
kbh0581 <kbh0581@gmail.com>
2021-04-29 23:54:22 +0900

    우아한 블로그 크롤링 WIP

--- scrapper/scrapper.go
+++ scrapper/scrapper.go
@@ -2,6 +2,8 @@ package scrapper
 
 import (
 	"log"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gocolly/colly"
@@ -14,14 +16,80 @@ type wohaTechDoc struct {
 	link   string
 }
 
-func RunScrapper() {
-	c := colly.NewCollector()
+func getMonthInt(month string) int {
+	if strings.LastIndex(month, "Jan") > -1 {
+		return 1
+	}
+	if strings.LastIndex(month, "Feb") > -1 {
+		return 2
+	}
+	if strings.LastIndex(month, "Mar") > -1 {
+		return 3
+	}
+	if strings.LastIndex(month, "Apr") > -1 {
+		return 4
+	}
+	if strings.LastIndex(month, "May") > -1 {
+		return 5
+	}
+	if strings.LastIndex(month, "Jun") > -1 {
+		return 6
+	}
+	if strings.LastIndex(month, "Jul") > -1 {
+		return 7
+	}
+	if strings.LastIndex(month, "Aug") > -1 {
+		return 8
+	}
+	if strings.LastIndex(month, "Sep") > -1 {
+		return 9
+	}
+	if strings.LastIndex(month, "Oct") > -1 {
+		return 10
+	}
+	if strings.LastIndex(month, "Nov") > -1 {
+		return 11
+	}
+	if strings.LastIndex(month, "Dec") > -1 {
+		return 12
+	}
+	return -1
 
-	defer c.Visit("https://woowabros.github.io/")
+}
+
+const Wowha = "Wowha"
+
+func RunScrapper() {
+	//컬리 생성
+	c := colly.NewCollector()
+	woowaRoot := "https://woowabros.github.io"
+	defer c.Visit(woowaRoot)
+
+	//html 찾기
 	c.OnHTML(".list", func(elList *colly.HTMLElement) {
 		elList.ForEach(".list-module", func(index int, el *colly.HTMLElement) {
+
 			doc := wohaTechDoc{}
-			doc.author = el.ChildText(".post-meta")
+			postMeta := el.ChildText(".post-meta")
+
+			meta := strings.Split(postMeta, ",")
+			println(el.ChildText(".post-meta"))
+			println(postMeta)
+
+			doc.author = strings.TrimSpace(meta[len(meta)-1])
+			year, _ := strconv.Atoi(strings.TrimSpace(meta[1]))
+			tempText := strings.Split(meta[0], " ")
+			month := getMonthInt(tempText[0])
+			day, _ := strconv.Atoi(tempText[1])
+
+			doc.time = time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+			doc.link = el.ChildAttr("a", "href")
+
+			//println(year, month, day)
+
+			//println(strings.Split(meta[0], " ")[0])
+
+			log.Print("wohaTechDoc:", doc)
 
 		})
 	})
